ce1e4d156eb2240d6c036698d19758d12f2662b8
isiko35 <95910857+isiko35@users.noreply.github.com>
2025-11-22 00:03:43 +0400

    Removed unnecessary reference files

--- basketcase/reference/flott/Flott HQ _ AI-Native Fleet Tracking, Built for Action._files/config.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
-  window._POSTHOG_REMOTE_CONFIG['phc_41kW9quMqpkJQlFAmW9P8hK7gi7fOpeoRvV29fswaOM'] = {
-    config: {"token": "phc_41kW9quMqpkJQlFAmW9P8hK7gi7fOpeoRvV29fswaOM", "supportedCompression": ["gzip", "gzip-js"], "hasFeatureFlags": false, "captureDeadClicks": false, "capturePerformance": {"network_timing": true, "web_vitals": true, "web_vitals_allowed_metrics": null}, "autocapture_opt_out": false, "autocaptureExceptions": false, "analytics": {"endpoint": "/i/v0/e/"}, "elementsChainAsString": true, "errorTracking": {"autocaptureExceptions": false, "suppressionRules": []}, "sessionRecording": {"endpoint": "/s/", "consoleLogRecordingEnabled": true, "recorderVersion": "v2", "sampleRate": null, "minimumDurationMilliseconds": null, "linkedFlag": null, "networkPayloadCapture": null, "masking": null, "urlTriggers": [], "urlBlocklist": [], "eventTriggers": [], "triggerMatchType": null, "scriptConfig": null}, "heatmaps": true, "surveys": false, "defaultIdentifiedOnly": true},
-    siteApps: []
-  }
-})();
